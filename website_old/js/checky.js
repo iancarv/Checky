39d6a7a1e63cc8b56cd2cb9887004ebf29516bbc
Ian Carvalho <iancarv@gmail.com>
2015-05-17 05:12:22 -0300

    weeeeb

--- website_old/js/checky.js
+++ website_old/js/checky.js
@@ -5,34 +5,6 @@
 $(function() {
 
   Parse.$ = jQuery;
-  window.fbAsyncInit = function() {
-        FB.init({
-          appId      : '407680106079301',
-          xfbml      : true,
-          version    : 'v2.3'
-        },  {scope: 'publish_actions,manage_pages,publish_pages',
-            enable_profile_selector: true});
-        FB.login(function(response) {
-           if (response.authResponse) {
-             console.log('Welcome!  Fetching your information.... ');
-             FB.api('/me', function(response) {
-               console.log('Good to see you, ' + response.name + '.');
-             });
-           } else {
-             console.log('User cancelled login or did not fully authorize.');
-           }
-         });
-      };
-
-      (function(d, s, id){
-         var js, fjs = d.getElementsByTagName(s)[0];
-         if (d.getElementById(id)) {return;}
-         js = d.createElement(s); js.id = id;
-         js.src = "http://connect.facebook.net/en_US/sdk.js";
-         fjs.parentNode.insertBefore(js, fjs);
-       }(document, 'script', 'facebook-jssdk'));
-
-
 
   // Initialize Parse with your Parse application javascript keys
   Parse.initialize("bbvNJHfvOXZ33SwTCYAq08pqe8MkmgALRI9NZC5f",
@@ -42,8 +14,7 @@ $(function() {
   // ----------
 
   // Our basic Todo model has `content`, `order`, and `done` attributes.
-  var Offer = Parse.Object.extend("Offer", 
-  {
+  var Offer = Parse.Object.extend("Offer", {
 
   });
   // Todo Collection
@@ -68,7 +39,6 @@ $(function() {
 
     // The DOM events specific to an item.
     events: {
-         "click .remove-offer" : "clear"
       // "click .toggle"              : "toggleDone",
       // "dblclick label.todo-content" : "edit",
       // "click .todo-destroy"   : "clear",
@@ -120,11 +90,10 @@ $(function() {
 
   // The main view that lets a user manage their todo items
   var ManageTodosView = Parse.View.extend({
+
+
     // Delegated events for creating new items, and clearing completed ones.
     events: {
-         "click #add-offer": "addOffer",
-         "click #triggerInput": "triggerInput",
-         "change input:file": "selectedFile"
       // "keypress #new-todo":  "createOnEnter",
       // "click #clear-completed": "clearCompleted",
       // "click #toggle-all": "toggleAllComplete",
@@ -157,72 +126,7 @@ $(function() {
     // Re-rendering the App just means refreshing the statistics -- the rest
     // of the app doesn't change.
     render: function() {
-      $('.addbox').html($('#add-template').html());
-      
-    },
-    triggerInput: function () {
-      $("#offer-img").click();
-    },
-    selectedFile: function () {
-      console.log("selectedFile");
-      var fileUploadControl = $("#offer-img")[0];
-      if (fileUploadControl.files.length > 0) {
-            $("#triggerInput").removeClass("hover-img");
-            var reader = new FileReader();
-
-            reader.onload = function (e) {
-                $('#addImg').attr('src', e.target.result);
-            }
-
-            reader.readAsDataURL(fileUploadControl.files[0]);
-      }
-
-    },
-    addOffer: function()
-    {
-      var self = this;
-      var fileUploadControl = $("#offer-img")[0];
-      if (fileUploadControl.files.length > 0) {
-              var file = fileUploadControl.files[0];
-              var name = "photo.jpg";
-              var parseFile = new Parse.File(name, file);
-              parseFile.save().then(function() {
-                self.title = self.$("#offer-title");
-                self.description = self.$("#offer-description");
-                var offer;
-                offer = self.offers.create({
-                  Title: self.title.val(),
-                  Description: self.description.val(),
-                  Image: parseFile,
-                  user:    Parse.User.current()
-                }, { success: function() { 
-                    console.log(offer);
-                    FB.api(
-                        "/1611227662447831/feed",
-                        "POST",
-                        {
-                           "caption": offer.get("Title"),
-                           "message": offer.get("Description"),
-                           "picture": parseFile.url(),
-                           "access_token": "CAAFyyGvnpEUBAPPNNuid2lBKtb2EIyrEnKvquEq2DWRQbPEsZBUpjKirQdWrFSmbwEw1S2zPAcziZBH32dQwmC8WTuFMDc0xdxWmEJPS6GWyjL3WCZCTmJspkZA2xdpOGvycxMVgyZCzZAP8KXl4MVM9dZB79PbFOUXmb2zKZA5DAlgZCtJlT1fGqA7bKysD8MIw3GQfuGOJm3jelZApe8zijx"
-                        },
-                        function (response) {
-                          if (response && !response.error) {
-                            /* handle the result */
-                            $("#triggerInput").addClass("hover-img");
-                            $('#addImg').attr('src', 'img/sign-27080_200.png');
-                            offer.set("PostID", response.id); console.log(response + ":" + response.id + "offer:" + offer.get("PostID"));
-                            offer.save();
-                          }
-                        });
-                  }
-                });
-                self.title.val(''); // ver essas linhas o que fazem
-                self.description.val('');
-              }, function(error) {
-                // The file either could not be read, or could not be saved to Parse.
-         });
-      }
+      // this.delegateEvents();
     },
 
     // Add a single todo item to the list by creating a view for it, and
@@ -236,12 +140,12 @@ $(function() {
       //   $(".content").append('<div class="row"></div>');
       // }
       var view = new OfferView({model: offer});
-      $(view.render().el).insertAfter($(".addbox"));
+      $(".row").append(view.render().el);
     },
 
     // Add all items in the Todos collection at once.
     addAll: function(collection, filter) {
-      $(".added").remove();
+      $(".row").html("");
       this.offers.each(this.addOne);
     }
   });
